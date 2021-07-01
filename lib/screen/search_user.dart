535bdca8bc1e152ceb1fc005d2851816bc831163
Nitikarn <nitikran12@gmail.com>
2021-07-01 20:24:52 +0700

    CREATE Screen search user

---
+++ lib/screen/search_user.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class SearchUserScreen extends StatefulWidget {
+  const SearchUserScreen({Key? key}) : super(key: key);
+
+  @override
+  _SearchUserScreenState createState() => _SearchUserScreenState();
+}
+
+class _SearchUserScreenState extends State<SearchUserScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("รายชื่อ"),
+      ),
+      body: 
+    );
+  }
+}
